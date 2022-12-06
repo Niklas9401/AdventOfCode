398bb48b9c1c6ab120a39dda60e049a7b9f19f5d
Niklas Weiblen <Niklas.Weiblen@focke.de>
2022-12-06 09:35:18 +0100

    Solved Day 1 of Dec 2015

---
+++ AdventOfCode/Years/Year_2015/Day 1/Day_1_2015.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Years
+{
+    public class Day_1_2015
+    {
+        public string INPUT_PATH =
+         Directory.GetParent(Environment.CurrentDirectory)
+         .Parent?.Parent?.FullName +
+         "/Years/Year_2015/Day 1/Input.txt";
+
+        public Day_1_2015() {
+            string Input = File.ReadAllText(INPUT_PATH);
+            char[] chars= Input.ToCharArray();
+            int floorCount = 0;
+            int basementEnteredPos = 0;
+            bool BasementWasEntered = false;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '(')
+                    floorCount++;
+                else
+                {
+                    floorCount--;
+                    if (floorCount < 0 && !BasementWasEntered)
+                    {
+                        BasementWasEntered= true;
+                        basementEnteredPos = i + 1;
+                    }
+                }
+            }
+            Console.WriteLine($"Santa is on floor: {floorCount}");
+            Console.WriteLine($"Santa gets to the basement at instruction number: {basementEnteredPos}");
+        }
+        
+    }
+}
